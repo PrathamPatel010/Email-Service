9cba84970dfbc235f6ce6341bd02c462dccca352
Pratham <ppratham812@gmail.com>
2024-01-15 13:01:34 +0530

    feat:web service developed

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 require('dotenv').config();
-
 const express = require('express');
+const {PORT, GMAIL_PASS,GMAIL_EMAIL} = require('./config/server-config');
+const mailsender = require('./config/email-config');
+const axios = require("axios");
+
 const app = express();
-const {GMAIL_EMAIL,GMAIL_PASS,PORT}=require('./config/server-config');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.listen(PORT,()=>{
     console.log(`Server is listening on port ${PORT}`);
-    console.log({GMAIL_EMAIL,GMAIL_PASS});
 });
+
+app.post('/api/v1/sendmail',async(req,res)=>{
+    console.log('Initiating Mail Service');
+    try{
+        const emailData = {
+            from:GMAIL_EMAIL,
+            to: process.env.RECEIVER_MAIL,
+            subject: 'Check If E-mail web Service is working',
+            text: "Yes, It's working. We can scale this project"
+        }
+        await mailsender.sendMail(emailData);
+        console.log('Mail Sent Successfully!!');
+        res.json({message:'Email Sent Successfully!!',data:emailData});
+    } catch (err){
+        console.log(err.message);
+    }
+})
